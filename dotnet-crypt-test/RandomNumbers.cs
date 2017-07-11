9ca2f4711e9dc5c7ec6377c1b381c8520a8ee398
Aleksey Berezan <aleksey.berezan@gmail.com>
2017-07-10 21:51:32 -0400

    Added rng sample

---
+++ dotnet-crypt-test/RandomNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+using NUnit.Framework;
+
+namespace dotnet_crypt_test
+{
+    [TestFixture]
+    public class RandomNumbers
+    {
+        private static byte[] GenerateRandomNumber(int bytesCount)
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                var randomBytes = new byte[bytesCount];
+                rng.GetBytes(randomBytes);
+                return randomBytes;
+            }
+        }
+
+        [Test]
+        public void Sample1()
+        {
+            byte[] r = null;
+            r = GenerateRandomNumber(4); Console.WriteLine(BitConverter.ToString(r) + ":" + Convert.ToBase64String(r));
+            r = GenerateRandomNumber(4); Console.WriteLine(BitConverter.ToString(r) + ":" + Convert.ToBase64String(r));
+            r = GenerateRandomNumber(4); Console.WriteLine(BitConverter.ToString(r) + ":" + Convert.ToBase64String(r));
+            r = GenerateRandomNumber(4); Console.WriteLine(BitConverter.ToString(r) + ":" + Convert.ToBase64String(r));
+            r = GenerateRandomNumber(4); Console.WriteLine(BitConverter.ToString(r) + ":" + Convert.ToBase64String(r));
+            r = GenerateRandomNumber(4); Console.WriteLine(BitConverter.ToString(r) + ":" + Convert.ToBase64String(r));
+        }
+    }
+}
