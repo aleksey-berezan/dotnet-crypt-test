0751d39d4c97a235ff92d8054533e6c269eb354a
Aleksey Berezan <aleksey.berezan@gmail.com>
2017-08-02 22:28:04 -0400

    Added PDKDF2 and SaltHashing

---
+++ dotnet-crypt-test/PBKDF2.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using NUnit.Framework;
+
+namespace dotnet_crypt_test
+{
+    [TestFixture]
+    public class PBKDF2
+    {
+        private static byte[] ComputeHash(string data, byte[] salt, int numberOfRounds)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(Encoding.Default.GetBytes(data), salt, numberOfRounds))
+            {
+                return rfc2898.GetBytes(32);
+            }
+        }
+
+        [Test]
+        public void Sample1()
+        {
+            int baseRounds = 50_000;
+            Utils.PrintBytes(Utils.Measure(() => ComputeHash("Hello World!", Utils.GenerateSalt(128), baseRounds * 1)));
+            Utils.PrintBytes(Utils.Measure(() => ComputeHash("Hello World!", Utils.GenerateSalt(128), baseRounds * 2)));
+            Utils.PrintBytes(Utils.Measure(() => ComputeHash("Hello World!", Utils.GenerateSalt(128), baseRounds * 3)));
+            Utils.PrintBytes(Utils.Measure(() => ComputeHash("Hello World!", Utils.GenerateSalt(128), baseRounds * 4)));
+            Utils.PrintBytes(Utils.Measure(() => ComputeHash("Hello World!", Utils.GenerateSalt(128), baseRounds * 5)));
+        }
+    }
+}
--- dotnet-crypt-test/Utils.cs
+++ dotnet-crypt-test/Utils.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace dotnet_crypt_test
@@ -9,7 +11,34 @@ namespace dotnet_crypt_test
 
         public static void PrintBytes(byte[] r) => Console.WriteLine($"{r.Length}" +
                                                                      $" :: {BitConverter.ToInt32(r, 0)}" +
-                                                                     $" :: {BitConverter.ToString(r)}" +
+                                                                     $" :: {BitConverter.ToString(r).Replace("-", "").ToLower()}" +
                                                                      $" :: {Convert.ToBase64String(r)}");
+
+        public static byte[] GenerateSalt(int length = 32)
+        {
+            const int saltLength = 32;
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                var bytes = new byte[saltLength];
+                rng.GetBytes(bytes);
+                return bytes;
+            }
+        }
+
+        public static byte[] Combine(byte[] data, byte[] salt)
+        {
+            var r = new byte[data.Length + salt.Length];
+            Buffer.BlockCopy(data, 0, r, 0, data.Length);
+            Buffer.BlockCopy(salt, 0, r, data.Length, salt.Length);
+            return r;
+        }
+
+        public static T Measure<T>(Func<T> f)
+        {
+            var sw = Stopwatch.StartNew();
+            var r = f();
+            Console.WriteLine("Elapsed time: {0}", sw.Elapsed);
+            return r;
+        }
     }
 }
